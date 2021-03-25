520950b624ff66f650ac9d28ff9752d6ae70b518
jonathanj05 <72229870+jonathanj05@users.noreply.github.com>
2021-03-25 10:24:31 -0500

    first commit

---
+++ area_mod.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def area_house (length, width):
+    area = width * length
+    return area
+
+def circumference_circle( 2, pi, radius):
+    circumference = 2* pi * radius
+print("circumference of a circle = %.2f" %circumference)
